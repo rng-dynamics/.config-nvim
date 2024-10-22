9b352c729e7ecbfc998c0feb037a832f27c4c00a
Alexander Scharinger <rng.dynamics@gmail.com>
2024-10-22 08:23:05 +0200

    update colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,7 @@
 -- local colorscheme = "moonlight"
 -- local colorscheme = "kanagawa"
-local colorscheme = "tokyonight"
+-- local colorscheme = "tokyonight"
+local colorscheme = "tokyonight-storm"
 
 local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not ok then
